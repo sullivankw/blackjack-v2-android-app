c185c7391363bdbf097f9f0bafdbb48f0e47bad4
Kevin Sullivan <sullivankw@gmail.com>
2019-01-12 21:51:37 -0800

    checking network connection before actions that require it

--- BlackjackHelper/app/src/main/java/com/sullivankw/blackjackhelper/AddToLeaderboardActivity.java
+++ BlackjackHelper/app/src/main/java/com/sullivankw/blackjackhelper/AddToLeaderboardActivity.java
@@ -1,7 +1,9 @@
 package com.sullivankw.blackjackhelper;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -80,6 +82,12 @@ public class AddToLeaderboardActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 if (name == null) {
+                    return;
+                }
+                if (!isNetworkConnected()) {
+                    Toast.makeText(getBaseContext(), "Connection unavailable. " +
+                            "Can't add to leaderboard at this time.", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(getBaseContext(), PracticeModeActivity.class));
                     return;
                 }
                 if (isValid) {
